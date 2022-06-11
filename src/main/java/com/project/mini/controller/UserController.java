217cd5a8621dc85dd2bfa9b5aef6436a73a2c540
leebyeounggwan <102587669+leebyeounggwan@users.noreply.github.com>
2022-06-11 09:23:00 +0900

    login

--- src/main/java/com/project/mini/controller/UserController.java
+++ src/main/java/com/project/mini/controller/UserController.java
@@ -2,31 +2,42 @@ package com.project.mini.controller;
 
 import com.project.mini.dto.JoinRequestDto;
 import com.project.mini.dto.LoginRequestDto;
+import com.project.mini.security.CustomLogoutSuccessHandler;
 import com.project.mini.security.jwt.JwtTokenProvider;
 import com.project.mini.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
+import java.io.IOException;
 
 @RestController
 @RequiredArgsConstructor
 public class UserController {
 
+    private final CustomLogoutSuccessHandler customLogoutSuccessHandler;
     private final UserService userService;
     private final JwtTokenProvider jwtTokenProvider;
 
+
     // 회원 로그인
+
     @PostMapping("/user/login")
-    public String login(@RequestBody LoginRequestDto loginRequestDto) {
+    public ResponseEntity login(final HttpServletResponse response, @RequestBody LoginRequestDto loginRequestDto) {
         if (userService.login(loginRequestDto)) {
             String token = jwtTokenProvider.createToken(loginRequestDto.getUsername());
             System.out.println(token);
-            return token;
+            response.addHeader("Authorization", token);
+            return new ResponseEntity<>("success", HttpStatus.OK);
         } else {
-            return "닉네임 또는 패스워드를 확인해주세요";
+//            response.sendError(100, "닉네임 또는 패스워드를 확인해주세요");
+            return new ResponseEntity<>("fail", HttpStatus.BAD_REQUEST);
         }
     }
 
@@ -40,6 +51,15 @@ public class UserController {
 
             return res;
         }
+    }
+
+    @GetMapping("/user/logout")
+    public void logout(HttpServletRequest request, HttpServletResponse response,
+                         Authentication authentication) throws ServletException, IOException {
+//        response.addHeader("Authorization", "");
+//
+//        return "redirect:/";
+        customLogoutSuccessHandler.onLogoutSuccess( request, response, authentication);
     }
 
     @PostMapping("/api/test")
